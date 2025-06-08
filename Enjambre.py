4408a915fb2bec1207ef57e4dce85036ec8c5c85
JavierGONL <JavierGONL@users.noreply.github.com>
2025-06-07 20:50:21 -0500

    new file:   Enjambre.py new file:   main.py

---
+++ Enjambre.py
@@ -0,0 +1,78 @@
+import random
+
+class Vector:
+    pass
+
+class Particle:
+    """
+    Cada partícula está definida por una posición, 
+    velocidad y valor que varían a medida que la partícula se mueve. 
+    Además, también almacena la mejor posición en la que ha estado hasta el momento. 
+    Cuando se crea aun nueva partícula, 
+    únicamente se dispone de información sobre su posición y velocidad (normalmente iniciada como cero), 
+    el resto de valores no se conocen hasta que la partícula es evaluada.
+    """
+
+    def __init__(self, posicion = [0,0], velocidad_inicial = [0,0]):
+        self.position = posicion
+        self.vel_i = velocidad_inicial
+        self.valor = 0 # no se si calcular esto aca en el enjambre
+        self.best_position = []
+        self.actual_position = []
+        self.maximice = False # min por defecto
+
+    def inicialice_particle():
+        """
+        definir su posicion y velocidad inicial
+        """
+        pass
+
+    def calculate_value(self, funcion):
+        
+        pass
+
+
+    def update_particle():
+        """
+        Mover una partícula implica actualizar su velocidad y posición. 
+        Este paso es el más importante ya que otorga al algoritmo la capacidad de optimizar.
+        
+        vi(t+1)=wvi(t)+c1r1[x^i(t)−xi(t)]+c2r2[g(t)−xi(t)] 
+        
+        donde:
+
+        vi(t+1): velocidad de la partícula i en el momento  t+1, es decir, la nueva velocidad.
+        vi(t): velocidad de la partícula  i en el momento  t, es decir, la velocidad actual.
+        w: coeficiente de inercia, reduce o aumenta a la velocidad de la partícula.
+        c1: coeficiente cognitivo.
+        r1: vector de valores aleatorios entre 0 y 1 de longitud igual a la del vector velocidad.
+        x^i(t): mejor posición en la que ha estado la partícula i hasta el momento.
+        xi(t): posición de la partícula  i en el momento  t.
+        c2: coeficiente social.
+        r2: vector de valores aleatorios entre 0 y 1 de longitud igual a la del vector velocidad.
+        g(t): posición de todo el enjambre en el momento t, el mejor valor global.
+        """
+        pass
+
+class swarm: #enjambre 
+    pass
+    def __init__(self, dimension = 2, number_of_particles = 0, dominio = []):
+        self.number_of_particles = number_of_particles
+        self.dominio = dominio
+        self.particulas = [0]*dimension
+        self.best_value = 0
+        self.best_position = [0]*dimension
+
+    def define_initial_vel_to_each_particle(self):
+        for i in self.particulas:
+            i.vel_i = random.uniform(0,1)
+    
+    def define_initial_pos_to_each_particle(self, funcion):
+        for i in self.particulas:
+            for j in i.position:
+                i.position[j] = random.uniform(self.dominio[0],self.dominio[1])
+            i.position[-1] = i.calculate_value(funcion)
+
+
+
+
