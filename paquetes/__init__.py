cfe031c130f383b7399a7f96a80a4b578e65df3f
imaluche <imaluche@unal.edu.co>
2025-06-11 07:49:55 -0500

    modified:   main.py 	modified:   paquetes/__init__.py 	new file:   paquetes/__pycache__/__init__.cpython-312.pyc 	modified:   paquetes/__pycache__/prueba_vector.cpython-312.pyc

--- paquetes/__init__.py
+++ paquetes/__init__.py
@@ -1,10 +1,11 @@
-from Enjambre import *
+#from Enjambre import *
 
-enjambre = Swarm(50, [-5.12, 5.12])
-enjambre.inicialize_each_particle()
-enjambre.update_particles(0.7, 1.5, 1.5)
-for i in enjambre.particulas:
-    print(i.value) 
+#enjambre = Swarm(50, [-5.12, 5.12])
+#enjambre.inicialize_each_particle()
+#enjambre.update_particles(0.7, 1.5, 1.5)
+#for i in enjambre.particulas:
+#    print(i.value) 
     
 #* por ahora crea las particulas bien y les da un valor de la funcion bien, 
 #* me falta ver la iteracion y comprobar la speed
+"Esta parte del codigo esta generando problemas, no me deja iniciar en el main"
