58124d9cff2fa8949944e7d2afbd13e7771cff16
David-A-Montes-R <158982203+David-A-Montes-R@users.noreply.github.com>
2025-06-11 19:52:50 -0500

    2 gráficas bonitas

--- paquetes/graficas.py
+++ paquetes/graficas.py
@@ -34,18 +34,44 @@ if __name__ == "__main__":
     # Mostrar posiciones iniciales de las partículas
     for idx, p in enumerate(swarm.particulas):
         print(f"Partícula {idx+1}: posición = {p.p_position}, velocidad = {p.speed}")
-        #* este es el ejemplo de matplotlib
 
     x = np.linspace(-5, 5,100)
     y = np.linspace(-5, 5,100)
     x, y = np.meshgrid(x,y) #hace el sistema de coordenadas
     z = np.cos(x) + np.cos(y) #funcion de X,Y
     
-    fig = plt.figure(figsize=(14,16))
-    ax = fig.add_subplot(111,projection = '3d')
+    fig = plt.figure(figsize=(16,12))
+    
+    ax = fig.add_subplot(2,2,1,projection = '3d')
     ax.plot_surface(x,y,z, cmap ='viridis')
     ax.set_title("gráfica 3D")
     ax.set_xlabel("eje X")
     ax.set_ylabel("eje Y")
     ax.set_zlabel("Eje Z")
+    
+    ax_2 = fig.add_subplot(2,2,2)
+    contour = ax_2.contourf(x, y, z, cmap ="viridis")
+    fig.colorbar(contour, ax = ax_2, shrink = 0.5, aspect = 5)
+    
+    ax_2.set_title("vista superior")
+    ax_2.set_xlabel("eje X")
+    ax_2.set_ylabel("eje Y")
+    # segundas gráficas
+    z = x**2 +y**2 #funcion de X,Y
+    ax_3 = fig.add_subplot(2,2,3,projection = '3d')
+    ax_3.plot_surface(x,y,z, cmap ='viridis')
+    ax_3.set_title("gráfica 3D 2")
+    ax_3.set_xlabel("eje X")
+    ax_3.set_ylabel("eje Y")
+    ax_3.set_zlabel("Eje Z")
+    
+    ax_4 = fig.add_subplot(2,2,4)
+    contour = ax_4.contourf(x, y, z, cmap ="viridis")
+    fig.colorbar(contour, ax = ax_2, shrink = 0.5, aspect = 5)
+    
+    ax_4.set_title("vista superior 2")
+    ax_4.set_xlabel("eje X")
+    ax_4.set_ylabel("eje Y")
+
+    plt.tight_layout()
     plt.show()
